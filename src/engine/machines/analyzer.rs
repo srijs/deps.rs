9b8c2f91b39614367f4c492c0f5e2814ebfe9268
Eduardo Pinho <enet4mikeenet@gmail.com>
2021-01-31 20:45:08 +0000

    Only query advisory database on latest matching
    Instead of checking the vulnerability database on each fetched release,
    defer checking to the finalization step,
    in which we already know the target's latest matching version

--- src/engine/machines/analyzer.rs
+++ src/engine/machines/analyzer.rs
@@ -23,12 +23,7 @@ impl DependencyAnalyzer {
         }
     }
 
-    fn process_single(
-        name: &CrateName,
-        dep: &mut AnalyzedDependency,
-        ver: &Version,
-        advisory_db: Option<&Database>,
-    ) {
+    fn process_single(_name: &CrateName, dep: &mut AnalyzedDependency, ver: &Version) {
         if dep.required.matches(&ver) {
             if let Some(ref mut current_latest_that_matches) = dep.latest_that_matches {
                 if *current_latest_that_matches < *ver {
@@ -36,18 +31,6 @@ impl DependencyAnalyzer {
                 }
             } else {
                 dep.latest_that_matches = Some(ver.clone());
-            }
-
-            let name: cargo_lock::Name = name.as_ref().parse().unwrap();
-            let version: cargo_lock::Version = ver.to_string().parse().unwrap();
-            let query = database::Query::new().package_version(name, version);
-
-            if let Some(db) = advisory_db {
-                let vulnerabilities = db.query(&query);
-                if !vulnerabilities.is_empty() {
-                    dep.vulnerabilities =
-                        vulnerabilities.into_iter().map(|v| v.to_owned()).collect();
-                }
             }
         }
         if !ver.is_prerelease() {
@@ -62,36 +45,48 @@ impl DependencyAnalyzer {
     }
 
     pub fn process<I: IntoIterator<Item = CrateRelease>>(&mut self, releases: I) {
-        let advisory_db = self.advisory_db.as_ref().map(|r| r.as_ref());
         for release in releases.into_iter().filter(|r| !r.yanked) {
             if let Some(main_dep) = self.deps.main.get_mut(&release.name) {
-                DependencyAnalyzer::process_single(
-                    &release.name,
-                    main_dep,
-                    &release.version,
-                    advisory_db,
-                )
+                DependencyAnalyzer::process_single(&release.name, main_dep, &release.version)
             }
             if let Some(dev_dep) = self.deps.dev.get_mut(&release.name) {
-                DependencyAnalyzer::process_single(
-                    &release.name,
-                    dev_dep,
-                    &release.version,
-                    advisory_db,
-                )
+                DependencyAnalyzer::process_single(&release.name, dev_dep, &release.version)
             }
             if let Some(build_dep) = self.deps.build.get_mut(&release.name) {
-                DependencyAnalyzer::process_single(
-                    &release.name,
-                    build_dep,
-                    &release.version,
-                    advisory_db,
-                )
+                DependencyAnalyzer::process_single(&release.name, build_dep, &release.version)
             }
         }
     }
 
-    pub fn finalize(self) -> AnalyzedDependencies {
+    fn process_advisory(&mut self) {
+        let advisory_db = self.advisory_db.as_ref().map(|r| r.as_ref());
+
+        // query advisory database for all latest matching dependencies
+        let main_deps = self.deps.main.iter_mut();
+        let dev_deps = self.deps.dev.iter_mut();
+        let build_deps = self.deps.build.iter_mut();
+        let deps = main_deps
+            .chain(dev_deps)
+            .chain(build_deps)
+            .filter_map(|(name, dep)| dep.latest_that_matches.clone().map(|v| (name, dep, v)));
+
+        for (name, dep, version) in deps {
+            let name: cargo_lock::Name = name.as_ref().parse().unwrap();
+            let version: cargo_lock::Version = version.to_string().parse().unwrap();
+            let query = database::Query::new().package_version(name, version);
+
+            if let Some(db) = advisory_db {
+                let vulnerabilities = db.query(&query);
+                if !vulnerabilities.is_empty() {
+                    dep.vulnerabilities =
+                        vulnerabilities.into_iter().map(|v| v.to_owned()).collect();
+                }
+            }
+        }
+    }
+
+    pub fn finalize(mut self) -> AnalyzedDependencies {
+        self.process_advisory();
         self.deps
     }
 }
